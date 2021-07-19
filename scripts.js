36c997b9ed42b314bee1979016fe0a2784f6d4df
wolwer <86207761+wolwerr@users.noreply.github.com>
2021-07-19 17:16:37 -0300

    Atualizando Códigos
    Melhorando a experiência do cliente.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,27 @@
 
+
 function setCredentials(event) {
     event.preventDefault();
-    var inputEmail = document.getElementById("email","nome");
-    localStorage.setItem("email","nome", inputEmail.value);
+
+    
+    
+    var inputNome = document.getElementById("nome");
+    var inputEmail = document.getElementById("email");
+    if (inputNome.value && inputEmail.value !== ""){
+    localStorage.setItem("nome", inputNome.value);
+    localStorage.setItem("email", inputEmail.value);
     document.getElementById("form").reset();
+        
+	console.log(`esse é o inputNome ${inputNome}`)
+	console.log(`esse é o inputDados ${inputEmail}`)
+	console.log(`local1 $localStorage.getItem('nome')`)
+	console.log(`local2 $localStorage.getItem('email')`);
+    window.alert("E-mail cadastrado com sucesso!!")
+
+}else{
+    window.alert("Preencha o nome e o E-mail")
+    
+}
 
 
 }
-
